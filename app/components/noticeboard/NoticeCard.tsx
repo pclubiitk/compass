83140f59d54b5a15258607b56bd9cf5ea63aeb33
Shivangtg <shivangtg857@gmail.com>
2025-06-03 01:08:28 +0530

    added sharing option

--- app/components/noticeboard/NoticeCard.tsx
+++ app/components/noticeboard/NoticeCard.tsx
@@ -1,3 +1,4 @@
+"use client"
 import * as React from "react"
 
 import { AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion"
@@ -5,32 +6,41 @@ import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/com
 
 
 
-export function NoticeCard({value,cardTitle,cardDescription,noticePreview,discription}:{value:string,cardTitle:string,cardDescription:string,noticePreview:string,discription: React.ReactNode}) {
+
+export function NoticeCard({id,cardTitle,cardDescription,noticePreview,discription}:{id:string,cardTitle:string,cardDescription:string,noticePreview:string,discription: React.ReactNode}) {
   return (
-    <AccordionItem value={value} className="border-0">
-      <Card className="overflow-hidden">
-        <CardHeader className="pb-0">
-          <CardTitle>{cardTitle}</CardTitle>
-          <CardDescription>{cardDescription}</CardDescription>
-        </CardHeader>
+      <AccordionItem id={id} value={id} className="border-0">
+        <Card className="overflow-hidden">
+          <CardHeader className="pb-0">
+            <CardTitle>{cardTitle}</CardTitle>
+            <CardDescription>{cardDescription}</CardDescription>
+          </CardHeader>
 
-        <CardContent className="pb-4">
-          <div className="space-y-2">
-            <p>{noticePreview}</p>
+          <CardContent className="pb-4">
+            <div className="space-y-2">
+              <p>{noticePreview}</p>
 
-            {/* Wrap Button in a div when using asChild */}
+              {/* Wrap Button in a div when using asChild */}
+                <div onClick={() => {
+                  if(window.location.hash.substring(1) !== id){
+                    window.location.hash = id;
+                    document.getElementById(id)?.scrollIntoView({ behavior: 'smooth' });
+                  }else{
+                  }
+                }}>
+                    <AccordionTrigger >
+                        Read More
+                    </AccordionTrigger>
+                </div>
+              </div>
 
-              <AccordionTrigger>
-                  Read More
-              </AccordionTrigger>
-            </div>
 
+            <AccordionContent className="pt-4 px-0">
+              {discription}
+            </AccordionContent>
 
-          <AccordionContent className="pt-4 px-0">
-            {discription}
-          </AccordionContent>
-        </CardContent>
-      </Card>
-    </AccordionItem>
+          </CardContent>
+        </Card>
+      </AccordionItem>
   )
 }
