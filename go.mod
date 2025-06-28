9290bed08bdbe0332bdfe8842764c07cb61ffd4c
Manas Jain Kuniya <mjlivetowork@gmail.com>
2025-06-28 12:10:17 +0530

    structuring: Correct the assignment location.

--- go.mod
+++
@@ -1,8 +0,0 @@
-module compass
-
-go 1.24.3
-
-require (
-	github.com/lib/pq v1.10.9
-	github.com/streadway/amqp v1.1.0
-)
--- server/cmd/models.go
+++
@@ -1,6 +0,0 @@
-package main
-
-type User struct {
-    ID       int    `json:"id"`
-    Username string `json:"username"`
-}
