4e0f0697b90d595bb75f390fb99fc88f828ecc8a
Manas Jain Kuniya <mjlivetowork@gmail.com>
2025-09-20 21:53:51 +0530

    correction: merge PR correction

--- server/connections/db.go
+++ server/connections/db.go
@@ -1,20 +1,18 @@
+// File for connecting the application to the postgres database
+// you may access the database anywhere in the application using connections.DB
 package connections
 
 import (
 	"compass/model"
 	"fmt"
-	"sync"
-	"time"
+
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-var (
-	DB   *gorm.DB
-	dbOnce sync.Once  //  added this to avoid re-calling the database connection multiple times
-)
+var DB *gorm.DB
 
 func dbConnection() {
 	host := viper.GetString("database.host")
@@ -26,39 +24,12 @@ func dbConnection() {
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Kolkata",
 		host, user, password, dbName, port)
 
-	var err error
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		logrus.Fatal("Failed to connect to database: ", err)
 	}
 
-	// setting the up connection pool
-	sqlDB, err := DB.DB()
-	if err != nil {
-		logrus.Fatal("Failed to get database instance: ", err)
-	}
-	
-	sqlDB.SetMaxIdleConns(10)
-	sqlDB.SetMaxOpenConns(100)
-	sqlDB.SetConnMaxLifetime(time.Hour)
-}
-
-func runMigrations() {
-	// this code snippet is added to handle role column migration first since Psql was throwing an error while database connection
-	if err := DB.Exec(`
-		DO $$
-		BEGIN
-			ALTER TABLE IF EXISTS users DROP CONSTRAINT IF EXISTS chk_users_role;
-			ALTER TABLE IF EXISTS users ALTER COLUMN role TYPE text;
-			ALTER TABLE IF EXISTS users ALTER COLUMN role TYPE varchar(10);
-			ALTER TABLE IF EXISTS users ADD CONSTRAINT chk_users_role CHECK (role IN ('admin', 'bot', 'user'));
-		EXCEPTION WHEN others THEN
-			RAISE NOTICE 'Migration error: %', SQLERRM;
-		END
-		$$;
-	`).Error; err != nil {
-		logrus.Warnf("Migration warning: %v", err)
-	}
+	DB = database
 
 	models := []interface{}{
 		&model.User{},
@@ -66,69 +37,13 @@ func runMigrations() {
 		&model.Notice{},
 		&model.Review{},
 		&model.Logs{},
+		&model.Image{},
 	}
 
 	if err := DB.AutoMigrate(models...); err != nil {
 		logrus.Fatal("Failed to auto-migrate models: ", err)
 	}
-
 	DB.Exec("CREATE EXTENSION IF NOT EXISTS pgcrypto")
 	DB.Exec("CREATE EXTENSION IF NOT EXISTS pg_trgm")
+	logrus.Info("Connected to database")
 }
-
-func InitDB() {
-	dbOnce.Do(func() {
-		dbConnection()
-		runMigrations()
-		logrus.Info("Database connection established")
-	})
-}
-// File for connecting the application to the postgres database
-// you may access the database anywhere in the application using connections.DB
-// package connections
-
-// import (
-// 	"compass/model"
-// 	"fmt"
-
-// 	"github.com/sirupsen/logrus"
-// 	"github.com/spf13/viper"
-// 	"gorm.io/driver/postgres"
-// 	"gorm.io/gorm"
-// )
-
-// var DB *gorm.DB
-
-// func dbConnection() {
-// 	host := viper.GetString("database.host")
-// 	port := viper.GetString("database.port")
-// 	password := viper.GetString("database.password")
-// 	dbName := viper.GetString("database.name")
-// 	user := viper.GetString("database.user")
-
-// 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Kolkata",
-// 		host, user, password, dbName, port)
-
-// 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-// 	if err != nil {
-// 		logrus.Fatal("Failed to connect to database: ", err)
-// 	}
-
-// 	DB = database
-
-// 	models := []interface{}{
-// 		&model.User{},
-// 		&model.Location{},
-// 		&model.Notice{},
-// 		&model.Review{},
-// 		&model.Logs{},
-// 		&model.Image{},
-// 	}
-
-// 	if err := DB.AutoMigrate(models...); err != nil {
-// 		logrus.Fatal("Failed to auto-migrate models: ", err)
-// 	}
-// 	DB.Exec("CREATE EXTENSION IF NOT EXISTS pgcrypto")
-// 	DB.Exec("CREATE EXTENSION IF NOT EXISTS pg_trgm")
-// 	logrus.Info("Connected to database")
-// }
--- server/connections/viper.go
+++ server/connections/viper.go
@@ -1,79 +1,30 @@
+// Load environment/config from YAML using Viper
 package connections
 
+// TODO: may be in run time i may need to change the configs,
+// or api keys how do i do it without again loading the envs
+
 import (
-	
-	"sync"
 	"github.com/sirupsen/logrus"
+
 	"github.com/spf13/viper"
 )
 
-var (
-	initOnce sync.Once
-)
-
-// this loads only from secrets.yml
 func viperConfig() {
-	initOnce.Do(func() {
-		// this configures Viper to only use secrets.yml
-		viper.SetConfigType("yaml")
-		viper.SetConfigName("secrets") 
-		viper.AddConfigPath(".")      
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("./")
 
-		
-		if err := viper.ReadInConfig(); err != nil {
-			
-			if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-				logrus.Fatal("secrets.yml file not found. Please create it from secrets.yml.template")
-			} else {
-				//errors
-				logrus.Fatalf("Error reading secrets.yml: %v", err)
-			}
-		}
-
-	})
-}
-
-// this safely retrieves a configuration value
-func GetConfig(key string) interface{} {
-	return viper.Get(key)
-}
-
-/// this gets the config variable values as a string and checks if it is  empty
-func GetStringConfig(key string) string {
-	val := viper.GetString(key)
-	if val == "" {
-		logrus.Warnf("Configuration key %s is empty", key)
+	viper.SetConfigName("config")
+	err := viper.ReadInConfig()
+	if err != nil {
+		logrus.Fatalf("Fatal error config file: %s \n", err)
+		panic(err)
 	}
-	return val
+
+	viper.SetConfigName("secret")
+
+	err = viper.MergeInConfig()
+	if err != nil {
+		logrus.Errorf("Fatal error secret file: %s \n", err)
+	}
 }
-
-// Load environment/config from YAML using Viper
-// package connections
-
-// // TODO: may be in run time i may need to change the configs,
-// // or api keys how do i do it without again loading the envs
-
-// import (
-// 	"github.com/sirupsen/logrus"
-
-// 	"github.com/spf13/viper"
-// )
-
-// func viperConfig() {
-// 	viper.SetConfigType("yaml")
-// 	viper.AddConfigPath("./")
-
-// 	viper.SetConfigName("config")
-// 	err := viper.ReadInConfig()
-// 	if err != nil {
-// 		logrus.Fatalf("Fatal error config file: %s \n", err)
-// 		panic(err)
-// 	}
-
-// 	viper.SetConfigName("secret")
-
-// 	err = viper.MergeInConfig()
-// 	if err != nil {
-// 		logrus.Errorf("Fatal error secret file: %s \n", err)
-// 	}
-// }
