9e5a44ab7d0d2ea0b793a3c7ebb78eb87c4c2697
Muragesh-24 <nyamagoudmurgesh@gmail.com>
2025-11-05 17:50:18 +0530

    fix : refresh token issues

--- server/auth/handler.login.go
+++ server/auth/handler.login.go
@@ -76,7 +76,8 @@ func loginHandler(c *gin.Context) {
 	}
 
 	// Creating JWT token
-	accessToken, refreshToken, err := middleware.GenerateToken(dbUser.UserID, int(dbUser.Role), dbUser.IsVerified)
+	accessToken, err := middleware.GenerateAccessToken(dbUser.UserID, int(dbUser.Role), dbUser.IsVerified);
+	refreshToken, err := middleware.GenerateRefreshToken(dbUser.UserID);
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to generate token"})
 		return
