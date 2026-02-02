a3d805076b9d74ffde855f539d26b122169bf514
Manas Jain Kuniya <mjlivetowork@gmail.com>
2026-02-03 00:49:34 +0530

    fix: pfp protection

--- search/src/lib/constant.ts
+++ search/src/lib/constant.ts
@@ -2,7 +2,7 @@
 // export const PROFILE_POINT = "http://localhost:3001/profile";
 // export const LOGIN_POINT = "http://localhost:3001/login";
 
-// In Production
+// FIXME(prod): In Production
 export const SEARCH_POINT = "https://search.pclub.in";
 export const PROFILE_POINT = "https://auth.pclub.in/profile";
 export const LOGIN_POINT = "https://auth.pclub.in/login";
