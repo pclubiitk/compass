725b0df4e70881edc3de494e1b3f4be7fd92b001
vineetnarwar7-321 <vineetnarwar7@gmail.com>
2026-03-12 23:14:03 +0530

    Remove search/src/lib/constant file

--- search/src/lib/constant.ts
+++
@@ -1,8 +0,0 @@
-export const SEARCH_POINT = "http://localhost:8083";
-export const PROFILE_POINT = "http://localhost:3001/profile";
-export const LOGIN_POINT = "http://localhost:3001/login";
-
-// In Production
-// export const SEARCH_POINT = "https://bsearch.pclub.in";
-// export const PROFILE_POINT = "https://bauth.pclub.in/profile";
-// export const LOGIN_POINT = "https://bauth.pclub.in/login";
