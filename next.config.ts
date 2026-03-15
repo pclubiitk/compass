644fe10a9c7435cc48604dfc9431baaf776750d4
riddhimatukliya <rtukliya25@iitk.ac.in>
2026-03-15 17:38:32 +0530

    fix(next): replace deprecated images.domains with images.remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,13 @@ const nextConfig: NextConfig = {
   /* config options here */
 
   images: {
-    domains: ["th.bing.com", "www.iitk.ac.in", "localhost", "shantsagar"],
+    remotePatterns: [
+      { protocol: "https", hostname: "th.bing.com" },
+      { protocol: "https", hostname: "www.iitk.ac.in" },
+      { protocol: "http", hostname: "localhost" },
+      { protocol: "https", hostname: "shantsagar" },
+    ],
   },
-  
 };
 
 export default nextConfig;
