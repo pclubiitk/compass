b154b84aa1c9b5fb50fab3a6a9aa92a98500a7e7
Prachi Jain <niajihcarp@gmail.com>
2026-07-07 21:10:00 +0530

    Fix: Dev/Prod Transition

--- search/src/lib/constant.ts
+++ search/src/lib/constant.ts
@@ -1,8 +1,13 @@
-// export const SEARCH_POINT = "http://localhost:8083";
-// export const PROFILE_POINT = "http://localhost:3001/profile";
-// export const LOGIN_POINT = "http://localhost:3001/login";
+const isDev = process.env.NODE_ENV === 'development';
 
-// FIXME(prod): In Production
-export const SEARCH_POINT = "https://search.pclub.in";
-export const PROFILE_POINT = "https://auth.pclub.in/profile";
-export const LOGIN_POINT = "https://auth.pclub.in/login";
+export const SEARCH_POINT = isDev 
+    ? "http://localhost:8083" // Dev
+    : "https://search.pclub.in";  // Prod
+
+export const PROFILE_POINT = isDev 
+    ? "http://localhost:3001/profile" // Dev
+    : "https://auth.pclub.in/profile"; // Prod
+
+export const LOGIN_POINT = isDev 
+    ? "http://localhost:3001/login" // Dev
+    : "https://auth.pclub.in/login"; // Prod
