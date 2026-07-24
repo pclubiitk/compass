e4be3f52f0bf853aca4e214d0d085a307ada96a0
Shubh <shubhsahu066@gmail.com>
2026-07-24 22:29:56 +0530

    add character limit on events

--- server/maps/request.model.go
+++ server/maps/request.model.go
@@ -33,7 +33,7 @@ func (r AddLocationRequest) ToLocation(userID uuid.UUID) model.Location {
 }
 
 type AddNoticeRequest struct {
-	Title        string     `json:"title" binding:"required"`
+	Title        string     `json:"title" binding:"required,max=50"`
 	Description  string     `json:"description" binding:"required"`
 	Body         string     `json:"body"`
 	CoverPic     *uuid.UUID `json:"coverPic"`
@@ -67,7 +67,7 @@ type FlagActionRequest struct {
 }
 
 type AddUserEventRequest struct {
-	Title                string     `json:"title" binding:"required"`
+	Title                string     `json:"title" binding:"required,max=50"`
 	Description          string     `json:"description"`
 	EventTime            time.Time  `json:"eventTime" binding:"required"`
 	EventEndTime         time.Time  `json:"eventEndTime" binding:"required"`
